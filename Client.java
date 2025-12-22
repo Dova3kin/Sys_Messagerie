afdfefcc89520854d3d828f028760a9754545638
Mathis <mathis.piault92@gmail.com>
2025-12-22 21:50:30 +0100

    faut tout refaire 0/20

--- Client.java
+++ Client.java
@@ -1,22 +1,24 @@
-import java.util.*;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
+import java.util.Scanner;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Client {
     private final String tel;
     private String nom;
     private String prenom;
     private String dossierPerso;
+    private BufferedReader reponseServeur = null;
+    private PrintWriter requeteServeur = null;
     private static String telRegex = "^0[0-9]{9}$";
     private static Pattern pattern = Pattern.compile(telRegex);
 
-    public Client(String tel) {
+    public Client(String tel) throws Exception {
         this.tel = tel;
         String dossierPerso = "CLIENT/" + tel;
         try {
@@ -24,23 +26,34 @@ public class Client {
             Files.createDirectory(path);
             System.out.println("Compte créé avec Succes... Connexion");
         } catch (FileAlreadyExistsException fae) {
-            System.out.println("Compte déjà existant !");
+            throw new Exception("Compte déjà existant !");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private void connect() {
+    private static boolean connect() {
         Socket soc = null;
         try {
             soc = new Socket("localhost", 7770);
-            BufferedReader buffReader = new BufferedReader(
-                    new InputStreamReader(soc.getInputStream()));
-            String ligne;
-            while ((ligne = buffReader.readLine()) != null)
-                System.out.println(ligne);
+            reponseServeur = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            requeteServeur = new PrintWriter(soc.getOutputStream(), true);
+            return true;
         } catch (IOException e) {
+            return false;
         }
+    }
+
+    private void sendRequest(String code) {
+        requeteServeur.println(getTel() + ":" + code);
+    }
+
+    private String getResponse() throws IOException {
+        return reponseServeur.readLine();
+    }
+
+    public String getTel() {
+        return tel;
     }
 
     public static void main(String[] args) {
@@ -51,31 +64,51 @@ public class Client {
         /**
          * Connexion au serveur ou création du compte
          */
-        while (!connecte) {
-            System.out.println("Veuillez rentrer votre identifiant (si vous êtes nouveau, rentrez \"n\"");
-            idConnexion = scan.nextLine();
-            if (idConnexion.equals("n")) {
-                System.out.println(
-                        "Bienvenue, veuillez rentrer votre numéro dé téléphone, celui-ci deviendra votre identifiant de connexions");
-                while (!connecte) {
-                    idConnexion = scan.nextLine();
-                    Matcher matcher = pattern.matcher(idConnexion);
-                    if (matcher.matches()) {
-                        Client c = new Client(idConnexion);
-                        connecte = true;
-                    } else {
-                        System.out.println("Veuillez donner un numéro de téléphone avec le format \"0XXXXXXXXX\"");
-                        try {
-                            Thread.sleep(500);
-                        } catch (Exception e) {
+        if (connect())
+            while (!connecte) {
+                System.out.println("Veuillez rentrer votre identifiant (si vous êtes nouveau, rentrez \"n\"");
+                idConnexion = scan.nextLine();
+
+                if (idConnexion.equals("n")) {
+                    System.out.println(
+                            "Bienvenue, veuillez rentrer votre numéro dé téléphone, celui-ci deviendra votre identifiant de connexions");
+
+                    while (!connecte) {
+                        idConnexion = scan.nextLine();
+                        Matcher matcher = pattern.matcher(idConnexion);
+
+                        if (matcher.matches()) {
+                            try {
+                                Client c = new Client(idConnexion);
+                                c.sendRequest("001");
+                                connecte = true;
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                                break;
+                            }
+                        } else {
+                            System.out.println("Veuillez donner un numéro de téléphone avec le format \"0XXXXXXXXX\"");
+                            try {
+                                Thread.sleep(500);
+                            } catch (Exception e) {
+                            }
+                            System.out.print("Numéro de téléphone : ");
                         }
-                        System.out.print("Numéro de téléphone : ");
                     }
+                } else {
+                    connecte = true;
                 }
-            } else {
-                connecte = true;
+                System.out.println("connecté");
             }
+        else {
+            System.out.println("Serveur hors connexion");
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+            System.out.println("Veuillez appeler le service de maintenance pour signaler le problème");
         }
 
     }
+
 }
--- Serveur.java
+++ Serveur.java
@@ -1,28 +1,45 @@
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 import java.io.*;
 
 class Serveur {
 
+    private static void analyse(String demande) {
+        String sender, code;
+        sender = demande.split(":")[0];
+        code = demande.split(":")[1];
+
+        if (code.equals("001")) { // 001 : création du compte client + connexion
+            String msg = sender;
+            try {
+                Files.write(Paths.get("SERVEUR/clients.txt"), msg.getBytes(), StandardOpenOption.CREATE,
+                        StandardOpenOption.APPEND);
+                System.out.println(sender + " connecté");
+            } catch (Exception E) {
+            }
+        } else if (code.equals("002")) // 002 : connexion
+            System.out.println(sender + " connecté");
+    }
+
     public static void main(String args[]) {
         ServerSocket server = null;
         Socket sock = null;
-        PrintWriter sockOut = null;
-        String str;
+        PrintWriter reponseServeur = null;
+        BufferedReader demandeClient = null;
+        String demande;
         Scanner sc = new Scanner(System.in);
         try {
             server = new ServerSocket(7770);
             while (true) {
                 sock = server.accept();
-                sockOut = new PrintWriter(
-                        sock.getOutputStream(), true);
-                BufferedReader sockIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                do {
-                    str = sc.nextLine();
-                    sockOut.println(str);
-                } while (!str.equals("q"));
-                sockOut.println("test");
-                sockOut.close();
+                demandeClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                reponseServeur = new PrintWriter(sock.getOutputStream(), true);
+                demande = demandeClient.readLine();
+                analyse(demande);
+                reponseServeur.close();
                 sock.close();
             }
         } catch (IOException e) {
